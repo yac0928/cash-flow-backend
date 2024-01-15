2aa7bd2bbdad0810ffc87a5ebb5e40e9d7b4b61a
yac0928 <kangarooinapark@gmail.com>
2024-01-15 11:47:51 +0800

    feat: build basic environment

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const routes = require('./routes')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(routes)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
+module.exports = app
