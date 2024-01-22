2ea821654d9d963935c2aa70628bcc11b17b5c8a
yac0928 <kangarooinapark@gmail.com>
2024-01-22 22:35:49 +0800

    feat: improve post-email on user.subscription

--- controller/email-controller.js
+++ controller/email-controller.js
@@ -1,31 +1,45 @@
 const nodemailer = require('nodemailer')
+const { Op } = require('sequelize')
+const { User, Subscription } = require('../models')
 
 const emailController = {
   postEmail: async (req, res, next) => {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS
+    try {
+      const users = await User.findAll({
+        include: [{
+          model: Subscription,
+          where: {
+            level: { [Op.not]: 'none' }
+          }
+        }]
+      })
+      if (!users) throw new Error('No users found')
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_PASS
+        }
+      })
+
+      await transporter.verify()
+      for (const user of users) {
+        const mailOptions = {
+          from: process.env.GMAIL_USER,
+          to: user.email,
+          subject: 'testSubject',
+          text: 'testText'
+        }
+        await transporter.sendMail(mailOptions)
       }
-    })
 
-    await transporter.verify()
-
-    const mailOptions = {
-      from: process.env.GMAIL_USER,
-      to: 'zxcv104302053@gmail.com',
-      subject: 'testSubject',
-      text: 'testText'
+      res.status(200).json({
+        status: 'success',
+        message: 'Emails sent successfully'
+      })
+    } catch (err) {
+      next(err)
     }
-
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        next(err)
-      } else {
-        res.json({ info })
-      }
-    })
   }
 }
 module.exports = emailController
