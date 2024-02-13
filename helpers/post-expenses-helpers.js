8afabcb057294c7e00573b2b358c000dd633386f
yac0928 <kangarooinapark@gmail.com>
2024-02-13 16:12:55 +0800

    feat: improve post-expense on last day of month

--- helpers/post-expenses-helpers.js
+++ helpers/post-expenses-helpers.js
@@ -4,7 +4,6 @@ const postNextFewYearsExpense = async (baseExpense, years) => {
   baseExpense = baseExpense.toJSON()
   const baseYear = baseExpense.date.getFullYear()
   const baseMonth = baseExpense.date.getMonth() // 0-11
-  const baseDate = baseExpense.date.getDate()
   const paymentPerMonth = parseInt(baseExpense.paymentPerMonth) // x個月扣款一次
   const amountOfExpenses = years * 12 / paymentPerMonth - 1 // 扣除第一次(已被加入)
   const newExpenses = [baseExpense]
@@ -14,11 +13,17 @@ const postNextFewYearsExpense = async (baseExpense, years) => {
   for (let i = 0; i < amountOfExpenses; i++) {
     currMonth += paymentPerMonth
     if (currMonth <= 11) {
-      currExpenseDate = new Date(Date.UTC(currYear, currMonth, baseDate))
+      // 检查当前月份的天数是否超过了新日期的天数
+      const daysInCurrMonth = new Date(currYear, currMonth + 1, 0).getDate()
+      const newDate = baseExpense.date.getDate() > daysInCurrMonth ? daysInCurrMonth : baseExpense.date.getDate()
+      currExpenseDate = new Date(Date.UTC(currYear, currMonth, newDate))
     } else {
       currMonth -= 12
       currYear += 1
-      currExpenseDate = new Date(Date.UTC(currYear, currMonth, baseDate))
+      // 检查当前月份的天数是否超过了新日期的天数
+      const daysInCurrMonth = new Date(currYear, currMonth + 1, 0).getDate()
+      const newDate = baseExpense.date.getDate() > daysInCurrMonth ? daysInCurrMonth : baseExpense.date.getDate()
+      currExpenseDate = new Date(Date.UTC(currYear, currMonth, newDate))
     }
     const newExpense = {
       ...baseExpense,
